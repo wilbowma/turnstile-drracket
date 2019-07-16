5bbf7d82c3260f6e3e41deb0d98d01724203cc57
William J. Bowman <wjb@williamjbowman.com>
2019-07-16 15:36:06 -0400

    Added typed holes

--- test.rkt
+++ test.rkt
@@ -1,5 +1,27 @@
 #lang turnstile
 (require (only-in turnstile/lang [⇒ =>] [⇐ <=] [≫ >>] [⊢ /-] [≻ >>>]))
+(require (only-in racket/base [error error-]))
+
+(define-base-type Hole)
+
+(define-typed-syntax ?
+  [(_ msg:str) >>
+   ---------------------
+   [⊢ #,(syntax-property #'(#%app- error- (#%datum- . msg))
+        'todo (syntax->datum #'msg)) => Hole]]
+  [_ >>
+   ---------------------
+   [⊢ (? "") => Hole]])
+
+(begin-for-syntax
+  (define old-relation (current-typecheck-relation))
+  ;; Every type is equal to 'Hole
+  (current-typecheck-relation
+   (lambda (t1 t2)
+     (syntax-parse (list t1 t2)
+       [(~Hole _) #t]
+       [(_ ~Hole) #t]
+       [_ ((old-relation) t1 t2)]))))
 
 (define-base-type Bool)
 (define-type-constructor -> #:arity > 0)
@@ -24,6 +46,8 @@
    ----------
    [#:error (type-error #:src #'e #:msg "Unsupported literal: ~v" #'e)]])
 
-((λ ([x : Bool]) x) #t)
+((λ ([x : Bool]) x) ?)
 
 #f
+
+(? "meow")
